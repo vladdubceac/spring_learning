4548e671e00a3746a3400b5c47e16338f15134e4
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-19 21:04:57 +0300

    Updated CRUD app

--- spring-crud-app1/src/main/java/md/vladdubceac/springcourse/controllers/PeopleController.java
+++ spring-crud-app1/src/main/java/md/vladdubceac/springcourse/controllers/PeopleController.java
@@ -5,9 +5,7 @@ import md.vladdubceac.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +29,16 @@ public class PeopleController {
         Person person =personDAO.show(id);
         model.addAttribute("person",person);
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person")Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
