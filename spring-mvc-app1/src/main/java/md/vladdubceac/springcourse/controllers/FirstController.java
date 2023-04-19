e99412880572b209056977198235771c16bbb9e1
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-19 11:35:30 +0300

    Added requests with parameters

--- spring-mvc-app1/src/main/java/md/vladdubceac/springcourse/controllers/FirstController.java
+++ spring-mvc-app1/src/main/java/md/vladdubceac/springcourse/controllers/FirstController.java
@@ -3,18 +3,26 @@ package md.vladdubceac.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false)String name, @RequestParam(value = "surname", required = false) String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
     }
 }
