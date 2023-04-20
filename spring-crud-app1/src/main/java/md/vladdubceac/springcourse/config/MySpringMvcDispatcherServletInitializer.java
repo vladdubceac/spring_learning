3dacff07973f63a1cf2a3ebfcf1399ce41a4b1ff
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-20 09:15:30 +0300

    Added 'Edit', 'Delete' functionalities

--- spring-crud-app1/src/main/java/md/vladdubceac/springcourse/config/MySpringMvcDispatcherServletInitializer.java
+++ spring-crud-app1/src/main/java/md/vladdubceac/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package md.vladdubceac.springcourse.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,15 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext){
+        aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
