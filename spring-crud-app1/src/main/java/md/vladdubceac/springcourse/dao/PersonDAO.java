991f322a5b5362c7030f806278204b8fcef07ee5
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-20 15:49:57 +0300

    Added external configuration file

--- spring-crud-app1/src/main/java/md/vladdubceac/springcourse/dao/PersonDAO.java
+++ spring-crud-app1/src/main/java/md/vladdubceac/springcourse/dao/PersonDAO.java
@@ -6,8 +6,6 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 @Component
