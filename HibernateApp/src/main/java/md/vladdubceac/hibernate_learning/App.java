5fd67fee4708332c929a417ff2345a37b33281d5
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-26 13:58:16 +0300

    Added methods in main() method for testing common operations

--- HibernateApp/src/main/java/md/vladdubceac/hibernate_learning/App.java
+++ HibernateApp/src/main/java/md/vladdubceac/hibernate_learning/App.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,27 +24,94 @@ public class App {
             session.beginTransaction();
 
             // get items belonging to a person
-
-            Person person = session.get(Person.class, 3);
-            System.out.println(person);
-
-            List<Item> items = person.getItems();
-
-            System.out.println(items);
+            // getPersonItems(session, 3);
 
             // get owner from item
+            // getOwnerFromItem(session, 5);
 
-            Item item = session.get(Item.class, 5);
-            System.out.println(item);
+            // Add new items
+            // addNewItems(session);
 
-            Person owner = item.getOwner();
-            System.out.println(owner);
+            // addNewPersonAndNewItem(session,new Person("Test person 2", 30), "Item from Hibernate 2");
 
+            // deletePersonItems(session, 3);
+
+            // deletePerson(session, 2);
+
+            updateItemOwner(session, 1, 4);
 
             session.getTransaction().commit();
         } finally {
             sessionFactory.close();
         }
 
+    }
+
+    private static void updateItemOwner(Session session, int itemId, int personId) {
+        Item item = session.get(Item.class, itemId);
+        item.getOwner().getItems().remove(item);
+        
+        Person person = session.get(Person.class, personId);
+
+        item.setOwner(person);
+        person.getItems().add(item);
+    }
+
+    private static void deletePerson(Session session, int personId) {
+        Person person = session.get(Person.class, personId);
+
+        // SQL
+        session.remove(person);
+
+        person.getItems().forEach(item -> item.setOwner(null));
+    }
+
+    private static void deletePersonItems(Session session, int personId) {
+        Person person = session.get(Person.class, personId);
+        List<Item> items = person.getItems();
+
+        // SQL
+        for(Item item: items){
+            session.remove(item);
+        }
+
+        // Doesn't generate SQL, but it's necessary to update the cache
+        person.getItems().clear();
+    }
+
+    private static void addNewPersonAndNewItem(Session session, Person person, String itemName) {
+        Item newItem = new Item(itemName, person);
+        person.setItems(new ArrayList<>(Collections.singletonList(newItem)));
+
+        session.save(person);
+
+        session.save(newItem);
+    }
+
+    private static void addNewItems(Session session) {
+        Person newItemOwner = session.get(Person.class, 2);
+
+        Item newItem = new Item("Item from Hibernate", newItemOwner);
+
+        newItemOwner.getItems().add(newItem);
+
+        session.save(newItem);
+    }
+
+    private static void getOwnerFromItem(Session session, int itemId) {
+        Item item = session.get(Item.class, itemId);
+        System.out.println(item);
+
+        Person owner = item.getOwner();
+        System.out.println(owner);
+    }
+
+    private static void getPersonItems(Session session, int personId) {
+        Person person = session.get(Person.class, personId);
+        System.out.println(person);
+
+        List<Item> items = person.getItems();
+
+        System.out.println(items);
     }
 }
