9a58deb613b87a8c95f2753a5505f09edce0746d
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-27 14:11:51 +0300

    Added new Spring + Hibernate project

---
+++ spring-mvc-hibernate-app1/src/main/java/md/vladdubceac/spring_learning/dao/PersonDAO.java
@@ -0,0 +1,57 @@
+package md.vladdubceac.spring_learning.dao;
+
+import md.vladdubceac.spring_learning.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("SELECT p FROM Person p",Person.class).getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public Person show(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Person personToBeUpdated = session.get(Person.class, id);
+
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Person.class,id));
+    }
+}
