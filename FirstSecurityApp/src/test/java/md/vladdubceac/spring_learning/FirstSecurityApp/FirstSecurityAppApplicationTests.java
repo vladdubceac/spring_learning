d3423995299cad5344c4366a5c9570d7cf6057ea
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-05-08 23:31:43 +0300

    Created Spring Boot + Spring security app

---
+++ FirstSecurityApp/src/test/java/md/vladdubceac/spring_learning/FirstSecurityApp/FirstSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package md.vladdubceac.spring_learning.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
