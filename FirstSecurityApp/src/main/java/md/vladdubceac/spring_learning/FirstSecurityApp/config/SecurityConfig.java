ec1598ebd5e02a3bbb28f07e096c24f2bd396172
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-06-02 23:42:46 +0300

    Added customized login page

--- FirstSecurityApp/src/main/java/md/vladdubceac/spring_learning/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/md/vladdubceac/spring_learning/FirstSecurityApp/config/SecurityConfig.java
@@ -1,22 +1,48 @@
 package md.vladdubceac.spring_learning.FirstSecurityApp.config;
 
-import md.vladdubceac.spring_learning.FirstSecurityApp.security.AuthProviderImpl;
+import md.vladdubceac.spring_learning.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService){
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // configure Spring Security
+        // configure authorization
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login","/auth/registration","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello",true)
+                .failureUrl("/auth/login?error");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
---
+++ FirstSecurityApp/src/main/java/md/vladdubceac/spring_learning/FirstSecurityApp/services/RegistrationService.java
@@ -0,0 +1,23 @@
+package md.vladdubceac.spring_learning.FirstSecurityApp.services;
+
+import md.vladdubceac.spring_learning.FirstSecurityApp.models.Person;
+import md.vladdubceac.spring_learning.FirstSecurityApp.repositories.PeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public RegistrationService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Transactional
+    public void register(Person person){
+        peopleRepository.save(person);
+    }
+}
